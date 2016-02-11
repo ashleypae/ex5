771be613d45096483a29999c1ebaa4d7519667ea
Ashley Pae <aypae@ucdavis.edu>
2016-02-10 23:23:11 -0800

    Initial commit

---
+++ ex5.js
@@ -0,0 +1,9 @@
+$.backstretch([
+          "images/water.jpg",
+          "images/lights.jpg",
+          "images/subway.jpg",
+          "images/park.jpg"
+        ], {
+            fade: 750,
+            duration: 4000
+        });
